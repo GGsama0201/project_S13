8c7d20d63751e10a9191b7448f0cedf27fca64b0
pukil326 <42304691+pukil326@users.noreply.github.com>
2024-09-19 03:42:54 -0400

    Add files via upload

---
+++ jewel.gd
@@ -0,0 +1,20 @@
+extends MeshInstance3D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if Input.get_action_strength("down"):
+		self.position.z+=0.1
+	elif Input.get_action_strength("up"):
+		self.position.z-=0.1
+	elif Input.get_action_strength("left"):
+		self.position.x-=0.1
+	elif Input.get_action_strength("right"):
+		self.position.x+=0.1
+	#elif Input.get_action_strength("jump"):
+		
